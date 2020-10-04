2c9c767051717afac5da8d2f03cdd58362afaf90
Anthony <2098507840@qq.com>
2020-10-04 10:30:41 +0800

    周报更新

---
+++ NLP/pdf2text.py
@@ -0,0 +1,35 @@
+from pdfminer.pdfparser import PDFParser
+# from pdfminer.pdfdocument import PDFDocument
+# from pdfminer.pdfpage import PDFPage
+# from pdfminer.pdfpage import PDFTextExtractionNotAllowed
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfdevice import PDFDevice
+import tkinter as tk
+from tkinter import filedialog
+
+root=tk.Tk()
+root.withdraw()
+file_path=filedialog.askopenfilename()
+print(file_path)
+
+
+# # Open a PDF file.
+# fp = open('mypdf.pdf', 'rb')
+# # Create a PDF parser object associated with the file object.
+# parser = PDFParser(fp)
+# # Create a PDF document object that stores the document structure.
+# # Supply the password for initialization.
+# document = PDFDocument(parser, password)
+# # Check if the document allows text extraction. If not, abort.
+# if not document.is_extractable:
+#     raise PDFTextExtractionNotAllowed
+# # Create a PDF resource manager object that stores shared resources.
+# rsrcmgr = PDFResourceManager()
+# # Create a PDF device object.
+# device = PDFDevice(rsrcmgr)
+# # Create a PDF interpreter object.
+# interpreter = PDFPageInterpreter(rsrcmgr, device)
+# # Process each page contained in the document.
+# for page in PDFPage.create_pages(document):
+#     interpreter.process_page(page)
