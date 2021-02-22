e5d72c12ba56444cb9881519e81598df212ed41e
anthony <2098507840@qq.com>
2021-02-22 20:54:17 +0800

    debug

--- NLP/Neo.py
+++ NLP/Neo.py
@@ -1,8 +1,12 @@
 import py2neo
+from requests import *
 from py2neo import Node, Relationship, Graph, NodeMatcher
 
 
 #连接上neo4j数据库
+from request import json2triad, getDBpedia
+
+
 def getConnection():
     graph = Graph(
         "http://localhost:7474",
@@ -33,14 +37,14 @@ def addAttributes(node,attrName,attrValue):
 #将数据写入数据库
 def writeToNeo4j(graph,a):
     matcher = NodeMatcher(graph)
-    a = matcher.match(getClass(a[0]), content=a[0]).first()
-    if (a == None):
-        a = Node(getClass(a[0]), content=a[0])
-    b = matcher.match(getClass(a[1]), content=a[1]).first()
-    if (b == None):
-        b = Node(getClass(a[1]), content=a[1])
-    r = Relationship(a, a[1], b)
-    s = a | b | r
+    x = matcher.match(getClass(a[0]), content=a[0]).first()
+    if (x == None):
+        x = Node(getClass(a[0]), content=a[0])
+    y = matcher.match(getClass(a[2]), content=a[2]).first()
+    if (y == None):
+        y = Node(getClass(a[2]), content=a[2])
+    r = Relationship(x, a[1], y)
+    s = x | y | r
     graph.create(s)
     return
 
@@ -49,5 +53,9 @@ def deleteAllData(graph):
     graph.run('match (n) detach delete n')
     return
 
-# graph=getConnection()
+graph=getConnection()
+deleteAllData(graph)
+data=json2triad(getDBpedia("宇宙学"))
+for item in data:
+    writeToNeo4j(graph, item)
 # deleteAllData(graph)
